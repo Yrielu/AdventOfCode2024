106d96f235f3ed83b9f736bb78e1e3b4b44308ae
Yrina <yrinaelur@gmail.com>
2025-01-30 13:03:49 +0100

    Solved day02 part 2

--- day02.py
+++ day02.py
@@ -2,7 +2,6 @@
 import pathlib
 import sys
 from itertools import count
-
 import numpy as np
 
 def parse(puzzle_input):
@@ -13,7 +12,6 @@ def parse(puzzle_input):
 
 def part1(data):
     """Solve part 1: Count the number of safe reports."""
-
     def isSafe(line):
         """Does checks in line and return True if passed all checks"""
         # Loop through levels/num
@@ -37,7 +35,38 @@ def part1(data):
 
 def part2(data):
     """Solve part 2."""
-    pass
+    def isSafe(line):
+        """Does checks in line and return True if passed all checks"""
+        # Loop through levels/num
+        prev_num = line[0]
+        diff_list = []
+        for num in line:
+            diff_list += [prev_num - num]
+            prev_num = num
+
+        all_incr_decre = all(i == np.sign(diff_list[1:])[0] for i in np.sign(diff_list[1:]))
+        # Check for steps bigger than 3
+        if any([abs(diff) > 3 for diff in diff_list]):
+            return False
+        # Check that levels are either all increasing or all decreasing
+        elif not all_incr_decre:
+            return False
+        return True
+
+    def problemDampener(line):
+        if isSafe(line):
+            return True
+        else:
+            count = 0
+            while count < len(line):
+                new_line = line[:count] + line[count + 1:]
+                if isSafe(new_line):
+                    return True
+                count += 1
+
+            return False
+
+    return len([report for report in data if problemDampener(report)])
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
